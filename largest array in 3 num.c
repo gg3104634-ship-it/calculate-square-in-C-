2073a44fec9d6b96114111c2df5dfd410afd8d95
gg3104634-ship-it <gg3104634@gmail.com>
2026-06-10 08:41:56 +0530

    Add files via upload

---
+++ largest array in 3 num.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void sum(int arr[], int n);
+
+int main(){
+    sum(arr[],5)
+
+return 0;
+}
+
+
+void sum(int arr[], int n){
+    int sum=0;
+    for(i=0; i<n; i++){
+        printf("number :",i);
+        sum++;
+    }
+
+    printf("sum of array",sum);
+}
+
+
